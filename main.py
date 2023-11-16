7989de238a268f911083d7d421c197b5e088f015
HuangPJ2022 <louie707024@gmail.com>
2023-11-16 13:06:05 +0800

    dash
    有時候連線會有問題，難以處理
    231116

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ from dash import Dash, html, dcc, Input, Output, callback
 from datetime import date, datetime, time, timedelta
 import openpyxl
 from dash.exceptions import PreventUpdate
+import time
 
 
 app = Dash(__name__)
@@ -25,7 +26,14 @@ app.layout = html.Div([
         min_date_allowed = date(2023, 11, 16),
         max_date_allowed = date(2023, 12, 31),
         initial_visible_month=date(2023, 11, 15),
-        date=date(2023, 11, 15)
+        date=date(2023, 11, 16)
+    ),
+    dcc.DatePickerSingle(
+        id = 'my-date-picker-checkOut',
+        min_date_allowed = date(2023, 11, 17),
+        max_date_allowed = date(2023, 12, 31),
+        initial_visible_month=date(2023, 11, 15),
+        date=date(2023, 11, 17)
     ),
     html.Button('Click here to see the image', id='show-secret'),
     dcc.Graph(id = 'scatter-plot')
@@ -34,18 +42,20 @@ app.layout = html.Div([
 
 @callback(
     Output(component_id='scatter-plot', component_property= 'figure'),
-    [Input('my-date-picker-checkIn', 'date'),Input('textarea', 'value'),Input('show-secret', 'n_clicks')])
+    [Input('textarea', 'value'),Input('my-date-picker-checkIn', 'date'),Input('my-date-picker-checkOut', 'date'),Input('show-secret', 'n_clicks')])
 
-def update_output(date_value, input_location, n_clicks):
+def update_output(input_location, date_value, date_out_value, n_clicks):
     if n_clicks is None:
         raise PreventUpdate
     else:
         date_object = date.fromisoformat(date_value)
         date_string = date_object.strftime('%Y-%m-%d')
 
-        date_out = datetime.strptime(date_value, '%Y-%m-%d') + timedelta(days=1)
+        start_time = time.time()
+
+        date_out = datetime.strptime(date_out_value, '%Y-%m-%d')
         print('start to scrap', date_value, date_out)
-        scrap(location = input_location, checkIn = date_value, checkOut = date_out.strftime('%Y-%m-%d'))
+        scrap(location = input_location, checkIn = date_value, checkOut = date_out.strftime('%Y-%m-%d'), start= start_time)
 
         #clean
         print('start to clean')
