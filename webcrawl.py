93bfc59f1fb967381333cb52ac584d9e426f3ec1
HuangPJ2022 <louie707024@gmail.com>
2023-11-21 10:59:10 +0800

    Update webcrawl.py
    change url

--- webcrawl.py
+++ webcrawl.py
@@ -23,21 +23,20 @@ def scrap(location, checkIn, checkOut, start):
     with sync_playwright() as p:
 
         #page_url = f'https://www.booking.com/searchresults.en-us.html?checkin={checkIn}&checkout={checkOut}&selected_currency=TWD&ss=London&ssne=London&ssne_untouched=London&lang=en-us&group_adults=2&no_rooms=1&group_children=0&sb_travel_purpose=leisure'
-
-        page_url = f'https://www.booking.com/searchresults.en-us.html?checkin={checkIn}&checkout={checkOut}&lang=en-us&group_adults=2&no_rooms=1&group_children=0&sb_travel_purpose=leisure'
+        page_url = f'https://www.booking.com/searchresults.en-us.html?checkin={checkIn}&checkout={checkOut}&ss={location}&lang=en-us&group_adults=2&no_rooms=1&group_children=0&sb_travel_purpose=leisure'
 
         browser = p.chromium.launch(headless=False)
         page = browser.new_page()
         page.goto(page_url, timeout=60000)
         
-        page.wait_for_selector('input[name=\"ss\"]')
+        """page.wait_for_selector('input[name=\"ss\"]')
         page.click('input[name=\"ss\"]')
         #page.locator('input[name=\"ss\"]').fill(location)
         
         with page.expect_navigation():
             page.keyboard.type(location)
             with page.expect_request_finished():
-                page.press('input[name=\"ss\"]', 'Enter')
+                page.press('input[name=\"ss\"]', 'Enter')"""
 
         totalHotel = page.locator("//div[@id='bodyconstraint-inner']").inner_text(timeout = 0)
         howManyPages = int(get_num_pages(totalHotel))
@@ -90,7 +89,4 @@ def scrap(location, checkIn, checkOut, start):
         browser.close()
 
 """if __name__ == '__main__':
-    scrap("Taichung", "2023-11-17", "2023-11-18")"""
-
-
-
+    scrap("Changhua", "2023-11-19", "2023-11-20", time.time())"""
